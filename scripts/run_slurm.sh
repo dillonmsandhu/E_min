a7cfee51e192a0473245fcd72bc0e1103f054233
Dillon Sandhu <dillonmsandhu@gmail.com>
2026-09-23 12:24:05 -0400

    slurm script for a single run

--- scripts/run_slurm.sh
+++ scripts/run_slurm.sh
@@ -5,22 +5,18 @@
 #SBATCH --partition compsci-gpu
 #SBATCH --gres=gpu:a5000:1 
 
-# Ensure working directory is the repository root
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-REPO_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
-cd "$REPO_ROOT"
+cd "$SLURM_SUBMIT_DIR"
 
-# Run like:
-# sbatch scripts/run_slurm.sh ppo.ppo_lstd mc '{"GAE_LAMBDA": 1.0}'
 START_TIME=$(date +"%Y-%m-%d %H:%M:%S")
-SECONDS=0  # start timer
+SECONDS=0
+
 FILE=$1
 SUFFIX=$2
 ENV=$3
 
 CONFIG="{\"NUM_ENVS\": 128, \"NUM_STEPS\": 64, \"MINIBATCH_SIZE\": 1024, \"TOTAL_TIMESTEPS\": 1000000, \"NUM_EPOCHS\": 4, \"GAE_LAMBDA\": 0.8, \"RETURN_LAMBDA\": 0.99, \"VALUE_LAMBDA\": 0.8, \"k\": 64, \"ENT_COEF\": 0.001,\"LAYER_NORM\": \"True\", \"SLURM_JOB_ID\": \"${SLURM_JOB_ID:-local}\", \"SLURM_ARRAY_JOB_ID\": \"${SLURM_ARRAY_JOB_ID:-local}\", \"SLURM_ARRAY_TASK_ID\": \"${SLURM_ARRAY_TASK_ID:-0}\", \"ACTOR_LR_END\": 0.0001, \"ACTOR_LR\": 0.003, \"CRITIC_LR\": 0.003,  \"CRITIC_LR_END\": 0.0001 }"
 
-CMD="/home/users/ds541/.pyenv/versions/3.10.15/envs/gymnax/bin/python ${FILE} --run-suffix ${SUFFIX} --config '${CONFIG}' --save-metrics --env-id ${ENV} --save-video --save-checkpoint --save-metrics"
+CMD="/home/users/ds541/.pyenv/versions/3.10.15/envs/gymnax/bin/python ${FILE} --run-suffix ${SUFFIX} --config '${CONFIG}' --save-metrics --env-ids ${ENV} --save-video --save-checkpoint"
 echo $CMD
 eval $CMD
 
